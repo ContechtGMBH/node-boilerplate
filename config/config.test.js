4e6527e8b9a68fbeb2708be71497b51ae43c1ee2
Damian Harasymczuk <harasymczuk@contecht.eu>
2018-08-20 14:39:48 +0200

    Update config.test.js

--- config/config.test.js
+++ config/config.test.js
@@ -2,6 +2,6 @@ var config = module.exports = {};
 
 config.hostname = 'http://localhost:3000';
 config.database = {
-  url: 'mongodb://test:test@ds245805.mlab.com:45805/testing',
+  url: 'mongodb://localhost:27017/dbname',
   secret: 'thisIsASecretToOurdatabase'
 }
